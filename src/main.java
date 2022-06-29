c307888c4ddda9e5d9e94e7dc5308d2d8ce5cef2
GerardoPazzz <gerardo.paz.an333@gmail.com>
2022-06-29 15:26:05 -0500

    Estoy modificando main

--- src/main.java
+++ src/main.java
@@ -13,7 +13,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola grupo 5");
     }
     
 }
