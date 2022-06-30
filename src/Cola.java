c421cbcfc2df4a6bbfcae383f2dd41364cf9aef0
KronosG6 <93363988+KronosG6@users.noreply.github.com>
2022-06-29 22:36:48 -0500

    Modificacion final

--- src/Cola.java
+++ src/Cola.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Pc
- */
+import javax.swing.JOptionPane;
+
 public class Cola {
 
     private Nodo inicioCola;
@@ -40,13 +34,13 @@ public class Cola {
         }
     }
 
-    public Nodo Extraer(/*String nombre, int num_asientos, int sala, int combo, double monto, */int num) {
+    public Nodo Extraer(String nombre, int num_asientos, int sala, int combo, double monto) {
         if (!ColaVacia()) {
- /*          inicioCola.nombre = nombre;
+            inicioCola.nombre = nombre;
             inicioCola.num_asientos = num_asientos;
             inicioCola.sala = sala;
             inicioCola.combo = combo;
-            inicioCola.monto = monto;*/
+            inicioCola.monto = monto;
             Nodo nodo_retirado = inicioCola;
             if (inicioCola == finalCola) {
                 inicioCola = null;
@@ -74,8 +68,21 @@ public class Cola {
         for (int i = cad.length - 1; i >= 0; i--) { // -1 es para evitar un desbordamiento de memoria
             ColaInvertida += " " + cad[i]; //para que no se amontonen los datos
         }
-        System.out.println(ColaInvertida);
+        JOptionPane.showMessageDialog(null, ColaInvertida);
         Cola = "";
     }
 
+    public void Eliminar() {
+        if (!ColaVacia()) {
+            if (inicioCola == finalCola) {
+                inicioCola = null;
+                finalCola = null;
+            } else {
+                inicioCola = inicioCola.siguiente;
+                Eliminar();
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "La cola esta vacia!");
+        }
+    }
 }
